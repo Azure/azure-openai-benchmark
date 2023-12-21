ec1c3b7086f49bfa1884b3a4e9cc25ecab2d4084
Michael Tremeer <michael_tremeer@hotmail.com>
2023-12-21 12:17:47 +1000

    match text to CLI arg

--- benchmark/statsaggregator.py
+++ benchmark/statsaggregator.py
@@ -102,8 +102,8 @@ class _StatsAggregator(threading.Thread):
             self.request_latency._append(stats.request_start_time, request_latency)
             if request_latency > self.window_duration:
                logging.warning((
-                     f"request completed in {math.ceil(request_latency)} seconds, while aggregation_window is {int(self.window_duration)} "
-                     "seconds. Consider increasing aggregation_window to at least 2x your typical request latency."
+                     f"request completed in {math.ceil(request_latency)} seconds, while aggregation-window is {int(self.window_duration)} "
+                     "seconds. Consider increasing aggregation-window to at least 2x your typical request latency."
                   )
                )   
             self.request_timestamps._append(stats.request_start_time, stats.request_start_time)
